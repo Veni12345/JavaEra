1b83030acd048327bd8de286eaba59676f0c9906
Veni12345 <lity_v@foxmail.com>
2022-07-29 15:48:21 +0800

    Java基础 String测试类

---
+++ src/Basics/StringTest.java
@@ -0,0 +1,23 @@
+package Basics;
+
+/**
+ * @author: Veni
+ * @date: 2022/07/29 七月 星期五 11:11
+ * @description:
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String a="1.";
+        String b="1.2.";
+        String c="1.2.";
+        String d="1.2.3";
+
+        boolean b1 = b.startsWith(a);
+        boolean b2 = b.startsWith(c);
+        boolean b3 = b.startsWith(d);
+        System.out.println(b1+" "+b2+" "+b3);
+    }
+
+
+
+}
