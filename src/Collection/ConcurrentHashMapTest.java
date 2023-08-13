5cf16995dc74fdfb685c8223929091e28e9540a5
Veni12345 <lity_v@foxmail.com>
2023-08-13 17:32:35 +0800

    ConcurrentHashMapTest

---
+++ src/Collection/ConcurrentHashMapTest.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author: Veni
+ * @date: 2023/08/13 八月 星期日 9:46
+ * @description:
+ */
+public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<>();
+        map.put(2,2);
+
+    }
+
+
+}
